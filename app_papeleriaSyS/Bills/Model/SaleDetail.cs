a82c03eefefba601bf42808e97f774005d9213c9
johelPotatil <sivets@unicauca.edu.co>
2023-01-17 13:12:42 -0500

    change in RepositoryProvider

--- app_papeleriaSyS/Bills/Model/SaleDetail.cs
+++ app_papeleriaSyS/Bills/Model/SaleDetail.cs
@@ -6,30 +6,38 @@ namespace app_papeleriaSyS.Bills.Model
     {
         #region attrubute
         private int id;
-        private readonly Product product;
+        private Product product;
         private int quantity;
         private float unit_price;
         private float total_price;
+        private int id_bill;
+        private bool state;
         #endregion
         #region constructor
         public SaleDetail()
         {
         }
-        public SaleDetail(int id, Product product, int quantity, float unit_price, float total_price)
+
+        public SaleDetail(int id, Product product, int quantity, float unit_price, float total_price, int id_bill, bool state)
         {
-            this.Id = id;
+            this.id = id;
             this.product = product;
-            this.Quantity = quantity;
-            this.Unit_price = unit_price;
-            this.Total_price = total_price;
+            this.quantity = quantity;
+            this.unit_price = unit_price;
+            this.total_price = total_price;
+            this.Id_bill = id_bill;
+            this.State = state;
         }
+
         #endregion
         #region getter and setter
         public int Id { get => id; set => id = value; }
-        public Product Product => product;
         public int Quantity { get => quantity; set => quantity = value; }
         public float Unit_price { get => unit_price; set => unit_price = value; }
         public float Total_price { get => total_price; set => total_price = value; }
+        public int Id_bill { get => id_bill; set => id_bill = value; }
+        public bool State { get => state; set => state = value; }
+        internal Product Product { get => product; set => product = value; }
         #endregion
     }
 }
--- app_papeleriaSyS/Users/Controller/UserController.cs
+++ app_papeleriaSyS/Users/Controller/UserController.cs
@@ -1,16 +1,41 @@
-﻿namespace app_papeleriaSyS.Users.Controller
+﻿using app_papeleriaSyS.Users.Model;
+using app_papeleriaSyS.Users.Repository;
+using System;
+
+namespace app_papeleriaSyS.Users.Controller
 {
     internal class UserController
     {
+        #region attribute
+        private RepositoryPerson repositoryPerson;
+        private RepositoryUser repositoryUser;
+        #endregion
 
-        public bool createUser(string username, string pass, string typeUser, int id, string name, string surname, int phone)
+        public  UserController()
         {
-
+            this.repositoryPerson = new RepositoryPerson();
+            this.repositoryUser = new RepositoryUser();
+        }
+        public bool createUser(string username, string pass, string typeUser, int id, string name, string surname, int phone, string email, DateTime birthdate)
+        {
+            User user= new User();
+            user.Username=username;
+            user.Password=pass; 
+            user.Surname=surname;
+            user.Phone=phone;
+            user.Email = email;
+            user.Birthdate = birthdate;
+            user.Id=id;
+            user.State = true;
+            repositoryPerson.createPerson(user);
+            repositoryUser.createUser(user);
 
 
 
             return true;
         }
+
+        
 
 
     }
